18cb95e8a61644c386ab5be8ecbda7a582fdec9c
Viacheslav Serhiienko <steelhouse.www.123@gmail.com>
2021-12-10 17:46:24 +0200

    update

--- src/store/index.ts
+++ src/store/index.ts
@@ -5,6 +5,7 @@ import { configureStore } from '@reduxjs/toolkit';
 import { rootReducer, RootState } from './root-reducer';
 import { routerMiddleware } from './history';
 
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 export const createStore = () =>
   configureStore({
     reducer: rootReducer,
